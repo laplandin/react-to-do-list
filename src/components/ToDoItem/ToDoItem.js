c6f63a8bdb8383d6dfbf593723f51c2cafd126e5
laplandin <deniskakhv@gmail.com>
2018-04-04 21:06:40 +0300

    added material ui and Redux dependencies

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton';
 import './ToDoItem.css'
+
+const style = {
+    margin: 12,
+    paddingRight: 5,
+    paddingLeft: 5
+};
 
 const ToDoItem = ({ task, index, removeTask, done }) => {
     return (
         <div className={task.status ? `to-do-item to-do-item--done` : `to-do-item`}>
             <h3>{index + 1} {task.name}</h3>
             <p>{task.description}</p>
-            <button onClick={function (e) {removeTask(index)}}>Удалить задачу</button>
-            <button onClick={(e) => done(index)}>Выполнено</button>
+            <RaisedButton onClick={function (e) {removeTask(index)}} style={style}>Удалить задачу</RaisedButton>
+            <RaisedButton onClick={(e) => done(index)} style={style}>Выполнено</RaisedButton>
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
@@ -7,7 +8,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
     </BrowserRouter>,
     document.getElementById('root')
 );
