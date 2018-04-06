8a238e9636df27c54214944e1988ad317124cf0e
laplandin <deniskakhv@gmail.com>
2018-04-07 00:02:22 +0300

    add about route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/header'
 import ToDoList from './components/ToDoList/ToDoList'
 import { Route } from 'react-router';
 import styles from './App.css';
+import About from './components/pages/about/about';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Header items={links} />
           <div id="main" className={styles.main}>
               <Route path="/to-do-list" component={ToDoList}/>
+              <Route path="/about" component={About}></Route>
           </div>
       </div>
     );
---
+++ src/components/pages/about/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about">
+            <h2>About page</h2>
+            <p>Some info about this awesome app</p>
+        </div>
+    )
+};
+
+export default About;
